eed130e8c02e593fdfd1b013d5b21e807cb6566b
Florian Bender <florian.bender@payone.de>
2016-12-14 16:02:59 +0100

    Initial source commit

---
+++ TestPlugin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TestPlugin;
+
+use GuzzleHttp\Psr7\ServerRequest;
+use Monolog\Logger;
+
+require_once 'vendor/autoload.php';
+
+class TestPlugin
+{
+    /** @var Logger */
+    protected $logger;
+
+    public function __construct()
+    {
+        $this->logger = new Logger('TestPluginLogger');
+    }
+
+    /**
+     * @param ServerRequest $someParameter
+     * @return array
+     */
+    public function doSomething(ServerRequest $someParameter)
+    {
+        $this->logger->log('something', 'something else');
+        return $someParameter->getHeader('X-Forwarded-For');
+    }
+}
